07a69b5979ca08c6909163cbd5b9c2441f00a2f0
Buyong Jo <byjo@nhnnext.org>
2014-09-23 07:52:13 +0900

    real final

--- js/googleplay_pc.js
+++ js/googleplay_pc.js
@@ -6,45 +6,38 @@
 	 * 변수 생성, 스타일 지정, 이벤트 등록을 한다 
 	 */
 	window.addEventListener('load', function(){
-			//setVariables();
-			var dShelf1 = document.getElementById('shelf1');
-			var dShelf2 = document.getElementById('shelf2');
-			var uSelectGenre = document.getElementById('select').querySelector('.genrelayer').querySelector('ul');
-			var dOverlay = document.getElementById('loading_overlay');
-			var uGenrelayer = document.querySelector('.genrelayer');
-			var uBooks1 = dShelf1.querySelector('.book');
-			var uBooks2 = dShelf2.querySelector('.book');
-			var dSeeMoreShelf1 = dShelf1.querySelector('div.more');
-			var dSeeMoreShelf2 = dShelf2.querySelector('div.more');
+		oVAR = setVariables({
+			dShelf1 : document.getElementById('shelf1'),
+			dShelf2 : document.getElementById('shelf2'),
+			uSelectGenre : document.getElementById('select').querySelector('.genrelayer').querySelector('ul'),
+			dOverlay : document.getElementById('loading_overlay'),
+			uGenrelayer : document.querySelector('.genrelayer'),
+			uBooks1 : document.getElementById('shelf1').querySelector('.book'),
+			uBooks2 : document.getElementById('shelf2').querySelector('.book'),
+			dSeeMoreShelf1 : document.getElementById('shelf1').querySelector('div.more'),
+			dSeeMoreShelf2 : document.getElementById('shelf2').querySelector('div.more')
+		});
 
-			var oDATA = {
-				sShelfHeight : "330px",
-				hTitleShelf1 : "새로나온 도서",
-				hTitleShelf2 : "베스트셀러"
-			}
+		oDATA = {
+			sShelfHeight : "330px",
+			hTitleShelf1 : "새로나온 도서",
+			hTitleShelf2 : "베스트셀러"
+		}
 
-			setElementStyle();
-			attachEvents();
+		setElementStyle();
+		attachEvents();
 	}, false);
 
-	// function setVariables(){
-	// 	var dShelf1 = document.getElementById('shelf1');
-	// 	var dShelf2 = document.getElementById('shelf2');
-	// 	var uSelectGenre = document.getElementById('select').querySelector('.genrelayer').querySelector('ul');
-	// 	var dOverlay = document.getElementById('loading_overlay');
-	// 	var uGenrelayer = document.querySelector('.genrelayer');
-	// 	var uBooks1 = dShelf1.querySelector('.book');
-	// 	var uBooks2 = dShelf2.querySelector('.book');
-	// 	var dSeeMoreShelf1 = dShelf1.querySelector('div.more');
-	// 	var dSeeMoreShelf2 = dShelf2.querySelector('div.more');
-
-	// 	var oDATA = {
-	// 		sShelfHeight : "330px",
-	// 		hTitleShelf1 : "새로나온 도서",
-	// 		hTitleShelf2 : "베스트셀러"
-	// 	}
-	// }
-
+	/* 
+	 * setVariables
+	 * object를 순회하며 변수 정의
+	 */
+	function setVariables(oEl){
+		for(var el in oEl){
+			oEl[el] = oEl[el];
+		}
+		return oEl;
+	}
 
 	/* 
 	 * setElementStyle
@@ -53,13 +46,13 @@
 	 * 첫 번째, 두 번째 서재의 높이 값 설정 sShelfHeight === 330px
 	 */
 	function setElementStyle(){
-		uGenrelayer.style.display = "none";
-		dOverlay.style.display = "none"
-		dSeeMoreShelf1.style.display = "block";
-		dShelf2.style.display = "block";
+		oVAR.uGenrelayer.style.display = "none";
+		oVAR.dOverlay.style.display = "none"
+		oVAR.dSeeMoreShelf1.style.display = "block";
+		oVAR.dShelf2.style.display = "block";
 
-		uBooks1.style.height = oDATA.sShelfHeight;
-		uBooks2.style.height = oDATA.sShelfHeight;
+		oVAR.uBooks1.style.height = oDATA.sShelfHeight;
+		oVAR.uBooks2.style.height = oDATA.sShelfHeight;
 	}
 
 	/* 
@@ -69,9 +62,9 @@
 	 * body영역 전체에 displayGenre 이벤트 등록
 	 */
 	function attachEvents(){
-		dSeeMoreShelf1.addEventListener('click', loadBooks, false);
-		dSeeMoreShelf2.addEventListener('click', loadBooks, false);
-		uSelectGenre.addEventListener('click', loadBooks, false);
+		oVAR.dSeeMoreShelf1.addEventListener('click', loadBooks, false);
+		oVAR.dSeeMoreShelf2.addEventListener('click', loadBooks, false);
+		oVAR.uSelectGenre.addEventListener('click', loadBooks, false);
 		document.body.addEventListener('click', displayGenre, false);
 	}
 
@@ -92,11 +85,11 @@
 	 * 
 	 */
 	function handleHeading(id){
-		var dHeadShelf1 = dShelf1.querySelector('.heading');
+		var dHeadShelf1 = oVAR.dShelf1.querySelector('.heading');
 		var hTitleShelf1 = dHeadShelf1.querySelector('h1');
 		var sSubTitleShelf1 = dHeadShelf1.querySelector('span');
 
-		var dHeadShelf2 = dShelf2.querySelector('.heading');
+		var dHeadShelf2 = oVAR.dShelf2.querySelector('.heading');
 		var hTitleShelf2 = dHeadShelf2.querySelector('h1');
 		var sSubTitleShelf2 = dHeadShelf2.querySelector('span');
 
@@ -104,23 +97,23 @@
 		switch(id){
 			case "shlef1_more" :
 				sSubTitleShelf1.innerHTML = "";
-				dSeeMoreShelf1.style.display = "none";
+				oVAR.dSeeMoreShelf1.style.display = "none";
 				break;
 
 			case "shlef2_more" :
 				hTitleShelf1.innerHTML = hTitleShelf2.innerHTML;
 				sSubTitleShelf1.innerHTML = "";
-				dSeeMoreShelf1.style.display = "none";
+				oVAR.dSeeMoreShelf1.style.display = "none";
 				break;
 
 			default :
 				hTitleShelf1.innerHTML = oDATA.hTitleShelf1;
 				sSubTitleShelf1.innerHTML = "";
-				dSeeMoreShelf1.style.display = "block";
+				oVAR.dSeeMoreShelf1.style.display = "block";
 
 				hTitleShelf2.innerHTML = oDATA.hTitleShelf2;
 				sSubTitleShelf2.innerHTML = "";
-				dSeeMoreShelf2.style.display = "block";
+				oVAR.dSeeMoreShelf2.style.display = "block";
 				break;
 			}
 	}
@@ -138,14 +131,14 @@
 			case "shlef1_more" :
 			// 첫 번째 서재의 더보기 클릭
 				getBookList(sListBestUrl);
-				dShelf2.style.display = "none";
+				oVAR.dShelf2.style.display = "none";
 				// 두 번째 서재는 보여주지 않는다
 				break;
 
 			case "shlef2_more" :
 			// 두 번째 서재의 더보기 클릭
 				getBookList(sListMovieUrl);
-				dShelf2.style.display = "none";
+				oVAR.dShelf2.style.display = "none";
 				break;
 
 			default :
@@ -153,8 +146,8 @@
 				sListGenreurl += className + ".json";
 				// 선택 된 장르에 맞는 데이터가 있는 url 주소 생성
 				getBookList(sListGenreurl);
-				if(dShelf2.style.display = "none")
-					dShelf2.style.display = "block";
+				if(oVAR.dShelf2.style.display = "none")
+					oVAR.dShelf2.style.display = "block";
 				break;
 		}
 	}
@@ -167,13 +160,13 @@
 		switch(tag){
 			case "DIV" :
 			// 더보기 클릭 시 모든 책의 list를 볼 수 있게 높이값 auto로 설정
-				uBooks1.style.height = "auto";
+				oVAR.uBooks1.style.height = "auto";
 				break;
 
 			case "LI" :
 			// 장르 클릭 시 각 서재가 1줄로 보여지도록 높이값을 330px로 설정
-				uBooks1.style.height = oDATA.sShelfHeight;
-				uBooks2.style.height = oDATA.sShelfHeight;
+				oVAR.uBooks1.style.height = oDATA.sShelfHeight;
+				oVAR.uBooks2.style.height = oDATA.sShelfHeight;
 				break;
 		}
 	}
@@ -192,18 +185,18 @@
 			if(request.readyState === 4 && request.status === 200) {
 				var result = request.responseText;
 				result = JSON.parse(result);
-				uBooks1.innerHTML = makeBookElement(result);
-				uBooks2.innerHTML = makeBookElement(result);
+				oVAR.uBooks1.innerHTML = makeBookElement(result);
+				oVAR.uBooks2.innerHTML = makeBookElement(result);
 				// 로딩 이미지 표시 시간이 너무 짧아 1초 후 없어지도록 설정
 				setTimeout(removeOveraly, 1000);
 			}
 		}
 
 		// Ajax 통신을 통해 data를 받아오는 동안 로딩 이미지를 표시한다
-		dOverlay.style.display = "block"
+		oVAR.dOverlay.style.display = "block"
 
 		function removeOveraly() {
-			dOverlay.style.display = "none";
+			oVAR.dOverlay.style.display = "none";
 		}
 	}
 
@@ -235,16 +228,16 @@
 	 */
 	function displayGenre(e){
 		// 장르 레이어가 숨겨져 있을 때
-		if(uGenrelayer.style.display === "none"){
+		if(oVAR.uGenrelayer.style.display === "none"){
 			if(e.target.className === "genre" || e.target.className === "genre_title" || e.target.className === "genre_icon")
-				uGenrelayer.style.display = "block"
+				oVAR.uGenrelayer.style.display = "block"
 		}
 
 		// 장르 레이어가 보일 때
 		else{
 			// 장르 레이어와 장르가 아닌 부분을 클릭하면 장르 레이러를 숨긴다
 			if(e.target.className !== "genrelayer")
-				uGenrelayer.style.display = "none"
+				oVAR.uGenrelayer.style.display = "none"
 		}
 	}
 	
